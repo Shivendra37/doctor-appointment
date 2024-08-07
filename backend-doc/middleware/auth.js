b332f650a8f8418147041b6955090de2e0544944
shivendra jha <93313563+HIVEND@users.noreply.github.com>
2024-08-07 22:54:45 +0545

    auth

--- backend-doc/middleware/auth.js
+++ backend-doc/middleware/auth.js
@@ -1,11 +1,14 @@
 const jwt = require('jsonwebtoken');
- 
+const Users = require("../model/user_model");
+
 const generateToken = (userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: '1h', // You can adjust the token expiration time as needed
   });
   return token;
 };
-module.exports = generateToken;
+
+
+module.exports =  generateToken ;
  
  
