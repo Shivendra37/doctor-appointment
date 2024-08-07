f95845d292d9518046103c5c27cfb69ad078de49
shivendra jha <93313563+HIVEND@users.noreply.github.com>
2024-08-07 22:57:41 +0545

    frontend updated

---
+++ frontend-doc/src/pages/User/Checkout.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Success = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <section class="row min-vh-100 align-items-center">
+        <div class="container py-5 d-flex justify-content-center">
+          <div class="col col-md-10 col-sm-12 col-lg-6">
+            <div class="card d-flex align-items-center shadow-lg">
+              <div class="row">
+                <div class="col-md-6 col-lg-6 order-md-1 order-lg-1 mb-4">
+                  <img
+                    src="https://i.pinimg.com/564x/5f/50/ec/5f50ecb76a6772969556d16a62c1fe5e.jpg"
+                    alt="success image"
+                    class="img-fluid"
+                    style={{ marginTop: 20 }}
+                  />
+                </div>
+
+                <div className="col-md-6 col-lg-6 order-lg-3 d-flex align-items-center">
+                  <div className="card-body p-5 p-lg-5 text-black">
+                    <form className="d-flex flex-column align-items-start">
+                      <div className="mb-4 d-flex justify-content-center">
+                        <i className="fas fa-cubes fa-2x me-2"></i>
+                      </div>
+                      <h4 style={{ color: "green" }}>Congratulations</h4>
+                      <strong> Successful Bought the package!!</strong>
+                      <p className="text-center mb-4">
+                        <strong>Hope you have a healthy check-up</strong>
+                      </p>
+                      <button
+                        className="btn w-70 mb-5 btn btn-dark"
+                        onClick={() => navigate("/healthpackages")}
+                      >
+                        Close
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Success;
--- frontend-doc/src/pages/User/Childcare.jsx
+++ frontend-doc/src/pages/User/Childcare.jsx
@@ -1,6 +1,6 @@
-// src/pages/User/Childcare.js
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../../components/Navbar"; // Import Navbar
 
 const Childcare = () => {
   const navigate = useNavigate();
@@ -10,68 +10,71 @@ const Childcare = () => {
   };
 
   return (
-    <div className="container">
-      <h1 style={{ paddingTop: "100px" }}>Childcare Information</h1>
-      <div className="row">
-        <div className="col-md-4">
-          <div className="card">
-            <img
-              src="../assets/images/childcare1.png"
-              className="card-img-top"
-              alt="Childcare Service 1"
-              style={{ height: "500px", objectFit: "cover" }}
-            />
-            <div className="card-body">
-              <h5 className="card-title">Child Nutrients</h5>
-              <button
-                className="btn btn-primary"
-                onClick={() => handleViewDetails(1)}
-              >
-                View Details
-              </button>
+    <>
+      <Navbar /> {/* Add Navbar component */}
+      <div className="container">
+        <h1 style={{ paddingTop: "100px" }}>Childcare Information</h1>
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <img
+                src="../assets/images/childcare1.png"
+                className="card-img-top"
+                alt="Childcare Service 1"
+                style={{ height: "500px", objectFit: "cover" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Child Nutrients</h5>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => handleViewDetails(1)}
+                >
+                  View Details
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card">
-            <img
-              src="../assets/images/childcare2.jpg"
-              className="card-img-top"
-              alt="Childcare Service 2"
-              style={{ height: "477px", objectFit: "cover" }}
-            />
-            <div className="card-body">
-              <h5 className="card-title">Child growth and development monitoring</h5>
-              <button
-                className="btn btn-primary"
-                onClick={() => handleViewDetails(2)}
-              >
-                View Details
-              </button>
+          <div className="col-md-4">
+            <div className="card">
+              <img
+                src="../assets/images/childcare2.jpg"
+                className="card-img-top"
+                alt="Childcare Service 2"
+                style={{ height: "477px", objectFit: "cover" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Child growth and development monitoring</h5>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => handleViewDetails(2)}
+                >
+                  View Details
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card">
-            <img
-              src="../assets/images/childcare3.jpg"
-              className="card-img-top"
-              alt="Childcare Service 3"
-              style={{ height: "500px", objectFit: "cover" }}
-            />
-            <div className="card-body">
-              <h5 className="card-title">Child Vaccine</h5>
-              <button
-                className="btn btn-primary"
-                onClick={() => handleViewDetails(3)}
-              >
-                View Details
-              </button>
+          <div className="col-md-4">
+            <div className="card">
+              <img
+                src="../assets/images/childcare3.jpg"
+                className="card-img-top"
+                alt="Childcare Service 3"
+                style={{ height: "500px", objectFit: "cover" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Child Vaccine</h5>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => handleViewDetails(3)}
+                >
+                  View Details
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
